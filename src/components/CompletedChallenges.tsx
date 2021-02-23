e19d97703cc629bfee6e4b1b2002d491da17a1c2
Leonardo Freire <lfreire80@gmail.com>
2021-02-23 18:10:07 -0300

    NLW4 Dia 2

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,10 @@
+import style from '../styles/components/CompletedChallenges.module.css'
+
+export function CompletedChallenges() {
+    return (
+        <div className={style.completedChallengessContainer}>
+            <span>Desafios completos</span>
+            <span>1</span>
+        </div>
+    )
+}
